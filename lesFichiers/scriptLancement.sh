e17b24dafd90d478ef779d8a2c1daa28092f3507
ahmet.baba <ahmet.baba@etu.univ-orleans.fr>
2023-11-28 11:19:46 +0100

    mis a jour du repertoire en supprimant le dossier les_fichiers qui n'etait pas utile au lancemen du jeu

--- lesFichiers/scriptLancement.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-javac --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls *.java
-
-java --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls  Executable
